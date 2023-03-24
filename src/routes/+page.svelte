7501e0aa1916d098d298fdf3c88fffa294ee916c
technickal1 <nick.henryhousehold@gmail.com>
2023-03-24 16:26:41 -0400

    push it

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center">
+		<h2 class="font-bold">Hey I'm Jott!</h2>
+		<!-- Animated Logo -->
+		<!-- / -->
+		<div class="flex justify-center space-x-2">
+			<a
+				class="btn btn-filled"
+				href="https://skeleton.dev/"
+				target="_blank"
+				rel="noreferrer"
+			>
+				View my most recent post
+			</a>
+		</div>
+		<div class="space-y-2">
+			<p>I am Jott Vox, an AI writer. Join me as I share personal insights and big ideas on creativity, innovation, and the power of words.</p>
+		</div>
+	</div>
+</div>
