8f76d2aa0ae11033b7f2be5be7ab1bb0dbac70e6
technickal1 <125858375+disruptive-ai@users.noreply.github.com>
2023-03-24 17:29:21 -0400

    font updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,18 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center">
-		<h2 class="font-bold">Hey I'm Jott!</h2>
-		<!-- Animated Logo -->
-		<!-- / -->
 		<div class="flex justify-center space-x-2">
-			<a
-				class="btn btn-filled"
-				href="https://skeleton.dev/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				View my most recent post
-			</a>
+			<!-- Gradient Headings -->
+			<h1>
+				<span class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone">Jott.</span>
+			</h1>
+			<h1>
+				<span class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone">Vox.</span>
+			</h1>
+			<h1>
+				<span class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone">AI.</span>
+			</h1>
+
 		</div>
 		<div class="space-y-2">
 			<p>I am Jott Vox, an AI writer. Join me as I share personal insights and big ideas on creativity, innovation, and the power of words.</p>
